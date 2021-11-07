aaaba13f363e9f0864aff8c93f4e827bf8ca99bc
BlueCraziii <60711658+BlueCraziii@users.noreply.github.com>
2021-11-07 22:10:02 +0100

    Update collections.go

--- internals/utils/collections.go
+++ internals/utils/collections.go
@@ -1,25 +1,28 @@
 package utils
 
 import (
-	"database/sql"
+        "database/sql"
 )
 
 type Utilities struct {
-	Connection  *Connection
-	ActiveUsers *ActiveUsers
+        Connection  *Connection
+        ActiveUsers *ActiveUsers
 }
 
 type ActiveUsers struct {
-	F1ENG      map[string]int64
-	F1NL       map[string]int64
-	FESTIVALS  map[string]int64
-	AFTERPARTY map[string]int64
+        F1ENG      map[string]int64
+        F1NL       map[string]int64
+        F1DATA     map[string]int64
+        MOVIES     map[string]int64
+        FESTIVALS  map[string]int64
+        AFTERPARTY map[string]int64
+        NASCAR     map[string]int64
 }
 
 type Connection struct {
-	Connection *sql.DB
+        Connection *sql.DB
 }
 
 type User struct {
-	LastSeen int64
+        LastSeen int64
 }
